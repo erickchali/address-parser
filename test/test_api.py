2f586a9181f8514a43113fd5c7b569fa051f7d5b
Erick Chali <erick.chali93@gmail.com>
2021-01-22 02:36:29 -0600

    Implemented context for tests, to allow db models to be created.

--- test/test_api.py
+++ test/test_api.py
@@ -1,19 +1,35 @@
 import pytest
-from flask import current_app
-from app import create_app
 
-app = create_app()
 healthcheck_endpoint = "/"
 parser_endpoint = "/address/parse"
 headers = {}
 
 
+@pytest.fixture(scope='session')
+def app(request):
+    from app import create_app
+    return create_app()
+
+
+@pytest.fixture(autouse=True)
+def app_context(app):
+    with app.app_context():
+        yield app
+
+
 @pytest.fixture
-def client():
-    with app.test_client() as client:
-        with app.app_context():
-            assert current_app.config["ENV"] == "production"
-        yield client
+def client(app_context):
+    return app_context.test_client()
+
+
+@pytest.fixture
+def db(app_context):
+    from database.db_models import db
+    db.create_all()
+    yield db
+    db.session.remove()
+    db.drop_all()
+    db.get_engine(app_context).dispose()
 
 
 def test_healthcheck(client):
@@ -44,7 +60,7 @@ def test_non_supported_address_format(client):
     assert response.json["message"] == "Unable to parse given address :("
 
 
-def test_supported_address_format(client):
+def test_supported_address_format(client, db):
     response = client.post(parser_endpoint, json={"address": "77, Bleecker Street"})
     assert response.status_code == 200
     assert response.json["street_name"] == "Bleecker Street"
